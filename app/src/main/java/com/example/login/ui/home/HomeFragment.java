b4b408184be43f58efbf19bf95315e3df79307a7
Anish <anishtimalsina747@gmail.com>
2023-04-04 10:46:13 +0545

    Implementing Videocall feature

--- app/src/main/java/com/example/login/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/login/ui/home/HomeFragment.java
@@ -1,17 +1,20 @@
 package com.example.login.ui.home;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
-
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
-
+import android.Manifest;
+import com.example.login.ConnectingActivity;
 import com.example.login.MainActivity;
 import com.example.login.databinding.FragmentHomeBinding;
 import com.google.firebase.auth.FirebaseAuth;
@@ -19,6 +22,8 @@ import com.google.firebase.auth.FirebaseAuth;
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
+    private int requestCode = 1;
+    String[] permissions = new String[]{Manifest.permission.RECORD_AUDIO, Manifest.permission.CAMERA};
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -34,9 +39,25 @@ public class HomeFragment extends Fragment {
             startActivity(new Intent(HomeFragment.this.getActivity(), MainActivity.class));
         });
 
+        Button callButton = binding.callBtn;
+        callButton.setOnClickListener(view -> {
+            isPermissionsGranted();
+            startActivity(new Intent(HomeFragment.this.getActivity(), ConnectingActivity.class));
+        });
+
         final TextView textView = binding.textHome;
         homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
+
+    }
+
+    private void isPermissionsGranted() {
+        for (String permission : permissions) {
+            if (ContextCompat.checkSelfPermission(HomeFragment.this.getActivity(), permission) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(HomeFragment.this.getActivity(), new String[]{permission}, requestCode);
+                //Toast.makeText(HomeFragment.this.getActivity(), "Permission Not Granted", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 
     @Override
